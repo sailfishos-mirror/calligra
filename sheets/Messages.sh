b31d4101855cba42ee1ab9fc8e9bf4b25c351c0c
Johnny Jazeix <jazeix@gmail.com>
2026-07-05 07:35:00 +0000

    Add missing files to the Sheets Messages.sh
    ui/actions/ and ui/actions/dialogs files were not translatable

--- sheets/Messages.sh
+++ sheets/Messages.sh
@@ -11,5 +11,5 @@ source ../kundo2_aware_xgettext.sh
 $EXTRACTRC --tag-group=koffice functions/*.xml >> rc.cpp
 $EXTRACTRC part/chart/*.ui part/dialogs/*.ui ui/dialogs/*.ui part/*.kcfg part/*.rc >> rc.cpp
 $EXTRACTATTR --attr=optionWidget,name ui/CellToolOptionWidgets.xml >> rc.cpp
-kundo2_aware_xgettext $potfilename *.cpp engine/*.cpp core/*.cpp core/ksp/*.cpp part/chart/*.cpp part/commands/*.cpp part/dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp part/commands/*.cpp part/dialogs/*.cpp ui/*.cpp ui/commands/*.cpp ui/dialogs/*.cpp ui/strategy/*.cpp
+kundo2_aware_xgettext $potfilename *.cpp engine/*.cpp core/*.cpp core/ksp/*.cpp part/chart/*.cpp part/commands/*.cpp part/dialogs/*.cpp functions/*.cpp part/AboutData.h part/*.cpp part/commands/*.cpp part/dialogs/*.cpp ui/*.cpp ui/actions/*.cpp ui/actions/dialogs/*.cpp ui/commands/*.cpp ui/dialogs/*.cpp ui/strategy/*.cpp
 rm -f rc.cpp
